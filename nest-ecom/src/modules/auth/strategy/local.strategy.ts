2355119f3ceec91015a880fb75c317acfe7b35be
pacificadora <singhalaman198@gmail.com>
2023-02-06 16:21:58 +0530

    implementing local strategy

--- nest-ecom/src/modules/auth/strategy/local.strategy.ts
+++ nest-ecom/src/modules/auth/strategy/local.strategy.ts
@@ -1,20 +1,26 @@
 /* eslint-disable prettier/prettier */
-import { UnauthorizedException } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-local";
-import { User } from "src/modules/users/user.entity";
-import { UsersService } from "src/modules/users/users.service";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { User } from 'src/modules/users/user.entity';
+import { UsersService } from 'src/modules/users/users.service';
 
-export class LocalStrategy extends PassportStrategy(Strategy){
-    constructor(private readonly userService: UsersService) {
-        super();
-    }
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UsersService) {
+    super();
+  }
 
-    async validate(username: string, password: string){
-        const user: User = await this.userService.getUserByUsername(username, password);
-        if (user) return user;
-        if (user == undefined) throw new UnauthorizedException('User Not found');
-        if (user.password != password)
-            throw new UnauthorizedException('Invalid password');
-    }
+  async validate(username: string, password: string) {
+    const user: User = await this.userService.getUserByUsername(
+      username,
+      password,
+    );
+    
+    
+    if (user) return user;
+    if (user == undefined) throw new UnauthorizedException('User Not found');
+    if (user.password != password)
+      throw new UnauthorizedException('Invalid password');
+  }
 }
